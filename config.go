315e47a9261f287fc06c4cd6fc48406aa3345299
amirrezaask <raskarpour@gmail.com>
2020-06-04 07:13:29 +0430

    new POC with bbolt

--- config.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "os"
-
-// ServerURL is the url of the server
-var ServerURL = os.Getenv("SERVER_URL", "localhost")
-
-// ServerPort is the server port
-var ServerPort = os.Getenv("SERVER_PORT", "8080")
--- main.go
+++ main.go
@@ -1,88 +1,81 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"os/signal"
+	"encoding/json"
+	"strings"
 
 	prompt "github.com/c-bata/go-prompt"
-	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
+	"go.etcd.io/bbolt"
 )
 
-func connect(addr string, userID string, deviceID string) (*websocket.Conn, error) {
-	c, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s?id=%s&deviceid=%s", addr, userID, deviceID), nil)
-	return c, err
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
-func executor(t string) {
-	if t == "bash" {
-		cmd := exec.Command("bash")
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
+var (
+	bucketName = []byte("passwords")
+)
+
+func commandHandler(ctx *context) func(string) {
+	return func(input string) {
+		cmds := strings.Split(input, " ")
+		switch cmds[0] {
+		case "get":
+			name := cmds[1]
+			ctx.db.View(func(tx *bbolt.Tx) error {
+				payload := tx.Bucket(bucketName).Get([]byte(name))
+				logrus.Println(payload)
+				return nil
+			})
+		case "set":
+			name := cmds[1]
+			username := cmds[2]
+			password := cmds[3]
+			ctx.db.Update(func(tx *bbolt.Tx) error {
+				bs, err := json.Marshal(map[string]string{
+					"username": username,
+					"password": password,
+					"name":     name,
+				})
+				if err != nil {
+					return err
+				}
+				bucket := tx.Bucket(bucketName)
+				if bucket == nil {
+					bucket, err = tx.CreateBucketIfNotExists(bucketName)
+					if err != nil {
+						return err
+					}
+				}
+				err = bucket.Put([]byte(name), bs)
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+		}
 	}
-	return
 }
 
-func completer(t prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{
-		{Text: "bash"},
-	}
+type context struct {
+	db *bbolt.DB
+}
+
+func openDatabase() (*bbolt.DB, error) {
+	return bbolt.Open("passage.db", 0600, nil)
 }
 
 func main() {
-	{
-		p := prompt.New(
-			executor,
-			completer,
-		)
-		p.Run()
-	}
-
-	addr := flag.String("addr", "localhost:8080", "server ws address")
-	userID := flag.String("userId", "1", "userid")
-	deviceID := flag.String("deviceId", "1", "device id")
-
-	flag.Parse()
-	log.SetFlags(0)
-
-	conn, err := connect(*addr, *userID, *deviceID)
+	db, err := openDatabase()
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatalln(err)
 	}
+	logrus.Infoln("Database Loaded")
+	defer db.Close()
+	ctx := &context{db: db}
+	p := prompt.New(commandHandler(ctx), completer)
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, os.Kill)
-
-	go handleIncomingEvent(conn)
-
-	for {
-		select {
-		case <-interrupt:
-			log.Println("Exiting ...")
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			return
-		}
-	}
-}
-
-func handleIncomingEvent(c *websocket.Conn) {
-	for {
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("error reading: ", err)
-			continue
-		}
-		log.Println(message)
-	}
+	p.Run()
 }
